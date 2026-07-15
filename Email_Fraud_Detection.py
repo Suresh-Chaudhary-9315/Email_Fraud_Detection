0123a15ba660630eff85b703508783be1c43df7c
Suresh-Chaudhary-9315 <sureshchaudhary9315@gmail.com>
2026-07-16 00:06:21 +0530

    Update numeric features for model input
    Removed 'is_internal_domain' from numeric features for model input.

--- Email_Fraud_Detection.py
+++ Email_Fraud_Detection.py
@@ -106,7 +106,7 @@ with tab2:
 
                 # Text Matrix Processing
                 X_text_live = tfidf.transform([body])
-                X_numeric_live = [[urls, body_len, subject_len, has_urgent_word, receiver_count, is_internal_domain]]
+                X_numeric_live = [[urls, body_len, subject_len, has_urgent_word, receiver_count]]
                 X_combined_live = sp.hstack((X_numeric_live, X_text_live))
                 st.write(f"Columns your model expects: {rf_model.n_features_in_}")
                 st.write(f"Columns you are sending right now: {X_combined_live.shape[1]}")
